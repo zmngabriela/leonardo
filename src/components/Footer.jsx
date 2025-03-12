fafadc7665a906504e96be24a2c175bd04311d84
zmngabriela <zmngabriela@gmail.com>
2025-03-12 09:52:11 +0100

    refactor: adding description content

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,7 +4,7 @@ function Footer() {
     return (
         <footer className="fixed-bottom flex row w-80 m-auto py-1 text-light">
             <div className="col-2 d-flex flex-column">
-                <p className="font-bold m-0">Content Creator</p>
+                <p className="font-bold m-0">Content producer, videomaker and photographer.</p>
                 <p className="m-0">Based in Barcelona</p>
             </div>
             <p className="col-1">&copy; {year}</p>
