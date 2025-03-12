ed281607ed2193eb24ea1c612f886fca5b66d9a0
zmngabriela <zmngabriela@gmail.com>
2025-03-12 12:40:20 +0100

    chore: installing and configuring react router dom and routes

--- src/App.js
+++ src/App.js
@@ -1,16 +1,31 @@
-import Footer from "./components/Footer";
-import Header from "./components/Header";
-import Home from "./pages/Home";
+import { BrowserRouter, Route, Routes } from "react-router-dom"
+
+import Home from "./pages/Home"
+import Paka from "./pages/Paka"
+import Nooda from "./pages/Nooda"
+import About from "./pages/About"
+import Contact from "./pages/Contact"
+
+import Layout from "./components/Layout"
+
 import './style.css'
 
 function App() {
   return (
     <>
-      <Header />
-      <Home />
-      <Footer />
+      <BrowserRouter>
+          <Routes>
+            <Route path="/" element={<Layout />}>
+              <Route index element={<Home />} />
+              <Route path="/paka" element={<Paka />} />
+              <Route path="/nooda" element={<Nooda />} />
+              <Route path="/about" element={<About />} />
+              <Route path="/contact" element={<Contact />} />
+            </Route>
+          </Routes>
+      </BrowserRouter>
     </>
   );
 }
 
-export default App;
+export default App
