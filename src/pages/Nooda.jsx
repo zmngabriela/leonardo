d6dd4dfff475dbacb70f71771ed0ea7f6e14491c
zmngabriela <zmngabriela@gmail.com>
2025-03-12 12:41:20 +0100

    feat: adding new pages

---
+++ src/pages/Nooda.jsx
@@ -0,0 +1,20 @@
+function Paka() {
+    return (
+        <section className="">
+            <div className="d-flex align-items-start align-items-md-center flex-column flex-md-row">
+                <video autoPlay muted loop className="d-block vh-50 vh-md-100 w-100 object-fit-cover">
+                    <source src="https://res.cloudinary.com/ddp4eyyfs/video/upload/v1741728107/Screen_Recording_2025-03-11_at_18.35.10_wxbrnz.mov" type="video/mp4" />
+                </video>
+                <p className="py-5 px-3">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ut minus itaque atque eaque voluptate alias voluptatem.</p>
+            </div>
+            <div className="d-flex align-items-start align-items-md-center flex-column flex-md-row">
+                <video autoPlay muted loop className="d-block vh-50 vh-md-100 w-100 object-fit-cover">
+                    <source src="https://res.cloudinary.com/ddp4eyyfs/video/upload/v1741728095/Screen_Recording_2025-03-11_at_18.36.20_tblypp.mov" type="video/mp4" />
+                </video>
+                <p className="py-5 px-3">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ut minus itaque atque eaque voluptate alias voluptatem.</p>
+            </div>
+        </section>
+    )
+}
+
+export default Paka
---
+++ src/pages/Paka.jsx
@@ -0,0 +1,25 @@
+function Paka() {
+    return (
+        <section className="">
+            <div className="d-flex align-items-start align-items-md-center flex-column flex-md-row">
+                <video autoPlay muted loop className="d-block vh-50 vh-md-100 w-100 object-fit-cover">
+                    <source src="https://res.cloudinary.com/ddp4eyyfs/video/upload/v1741728099/Screen_Recording_2025-03-11_at_18.27.01_thbeg8.mov" type="video/mp4" />
+                </video>
+                <p className="py-5 px-3">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ut minus itaque atque eaque voluptate alias voluptatem.</p>
+            </div>
+            <div className="d-flex align-items-start align-items-md-center flex-column flex-md-row">
+                <video autoPlay muted loop className="d-block vh-50 vh-md-100 w-100 object-fit-cover">
+                    <source src="https://res.cloudinary.com/ddp4eyyfs/video/upload/v1741728099/Screen_Recording_2025-03-11_at_18.29.37_zbxcoo.mov" type="video/mp4" />
+                </video>
+                <p className="py-5 px-3">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ut minus itaque atque eaque voluptate alias voluptatem.</p>
+            </div>
+            <div>
+                <video autoPlay muted loop className="d-block vh-50 vh-md-100 w-100 object-fit-cover">
+                    <source src="https://res.cloudinary.com/ddp4eyyfs/video/upload/v1741728088/Screen_Recording_2025-03-11_at_18.27.58_fvosvk.mov" type="video/mp4" />
+                </video>
+            </div>
+        </section>
+    )
+}
+
+export default Paka
