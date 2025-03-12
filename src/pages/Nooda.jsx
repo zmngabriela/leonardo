e5e1813b6ecf5c4d0fc0ad763aaab342d57591e8
zmngabriela <zmngabriela@gmail.com>
2025-03-12 13:15:55 +0100

    style: improving style layout

--- src/pages/Nooda.jsx
+++ src/pages/Nooda.jsx
@@ -1,20 +1,20 @@
-function Paka() {
+function Nooda() {
     return (
-        <section className="">
-            <div className="d-flex align-items-start align-items-md-center flex-column flex-md-row">
-                <video autoPlay muted loop className="d-block vh-50 vh-md-100 w-100 object-fit-cover">
+        <section>
+            <div className="row align-items-start align-items-md-center">
+                <video autoPlay muted loop className="d-block col-12 col-md-6 object-fit-cover p-0">
                     <source src="https://res.cloudinary.com/ddp4eyyfs/video/upload/v1741728107/Screen_Recording_2025-03-11_at_18.35.10_wxbrnz.mov" type="video/mp4" />
                 </video>
-                <p className="py-5 px-3">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ut minus itaque atque eaque voluptate alias voluptatem.</p>
+                <p className="col-12 col-md-6 py-5 px-3">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ut minus itaque atque eaque voluptate alias voluptatem.</p>
             </div>
-            <div className="d-flex align-items-start align-items-md-center flex-column flex-md-row">
-                <video autoPlay muted loop className="d-block vh-50 vh-md-100 w-100 object-fit-cover">
+            <div className="row align-items-start align-items-md-center">
+                <video autoPlay muted loop className="d-block col-12 col-md-6 object-fit-cover p-0">
                     <source src="https://res.cloudinary.com/ddp4eyyfs/video/upload/v1741728095/Screen_Recording_2025-03-11_at_18.36.20_tblypp.mov" type="video/mp4" />
                 </video>
-                <p className="py-5 px-3">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ut minus itaque atque eaque voluptate alias voluptatem.</p>
+                <p className="col-12 col-md-6 py-5 px-3">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ut minus itaque atque eaque voluptate alias voluptatem.</p>
             </div>
         </section>
     )
 }
 
-export default Paka
+export default Nooda
--- src/pages/Paka.jsx
+++ src/pages/Paka.jsx
@@ -1,20 +1,20 @@
 function Paka() {
     return (
         <section className="">
-            <div className="d-flex align-items-start align-items-md-center flex-column flex-md-row">
-                <video autoPlay muted loop className="d-block vh-50 vh-md-100 w-100 object-fit-cover">
+            <div className="row align-items-start align-items-md-center">
+                <video autoPlay muted loop className="d-block col-12 col-md-6 object-fit-cover p-0">
                     <source src="https://res.cloudinary.com/ddp4eyyfs/video/upload/v1741728099/Screen_Recording_2025-03-11_at_18.27.01_thbeg8.mov" type="video/mp4" />
                 </video>
-                <p className="py-5 px-3">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ut minus itaque atque eaque voluptate alias voluptatem.</p>
+                <p className="col-12 col-md-6 py-5 px-3">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ut minus itaque atque eaque voluptate alias voluptatem.</p>
             </div>
-            <div className="d-flex align-items-start align-items-md-center flex-column flex-md-row">
-                <video autoPlay muted loop className="d-block vh-50 vh-md-100 w-100 object-fit-cover">
+            <div className="row align-items-start align-items-md-center">
+                <video autoPlay muted loop className="d-block col-12 col-md-6 object-fit-cover p-0">
                     <source src="https://res.cloudinary.com/ddp4eyyfs/video/upload/v1741728099/Screen_Recording_2025-03-11_at_18.29.37_zbxcoo.mov" type="video/mp4" />
                 </video>
-                <p className="py-5 px-3">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ut minus itaque atque eaque voluptate alias voluptatem.</p>
+                <p className="col-12 col-md-6 py-5 px-3">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ut minus itaque atque eaque voluptate alias voluptatem.</p>
             </div>
             <div>
-                <video autoPlay muted loop className="d-block vh-50 vh-md-100 w-100 object-fit-cover">
+                <video autoPlay muted loop className="d-block col-12 col-md-6 object-fit-cover p-0">
                     <source src="https://res.cloudinary.com/ddp4eyyfs/video/upload/v1741728088/Screen_Recording_2025-03-11_at_18.27.58_fvosvk.mov" type="video/mp4" />
                 </video>
             </div>
