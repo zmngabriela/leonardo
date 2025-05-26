c3a54618bff1504daf2253f40b5dd8b2711a6919
zmngabriela <zmngabriela@gmail.com>
2025-05-26 12:59:40 +0200

    style: adding footer preloader animation

--- src/layout/Footer.jsx
+++ src/layout/Footer.jsx
@@ -1,9 +1,20 @@
+import { useEffect, useState } from "react";
+
 function Footer() {
     const year = new Date().getFullYear()
+    const [isVisible, setIsVisible] = useState(false);
     
+    // effect to show the footer after the preloader
+    useEffect(() => {
+        const timer = setTimeout(() => {
+            setIsVisible(true);
+        }, 3000);
+        return () => clearTimeout(timer);
+    }, []);
+
     return (
         <footer 
-            className="fixed-bottom gap-5 d-flex pb-4 px-4"
+            className={`fixed-bottom gap-5 d-flex pb-4 px-4 ${isVisible ? 'preloader-footer' : 'd-none'}`}
             style={{ mixBlendMode: 'difference' }}
         >
             <div className="w-50 w-md-25 d-flex flex-column">
