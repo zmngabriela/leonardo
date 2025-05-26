b24c55c593cf316036e0037f70729cd834bd0883
zmngabriela <zmngabriela@gmail.com>
2025-05-26 13:00:16 +0200

    feat: adding main text preloader animation

--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -1,4 +1,16 @@
+import { useEffect, useState } from "react";
+
 const Hero = ({ backgroundColor, children }) => {
+    const [isVisible, setIsVisible] = useState(false);
+        
+        // effect to show the footer after the preloader
+        useEffect(() => {
+            const timer = setTimeout(() => {
+                setIsVisible(true);
+            }, 2500);
+            return () => clearTimeout(timer);
+        }, []);
+
     return (
         <section 
             className="hero w-100 d-flex justify-content-center align-items-center"
@@ -7,7 +19,7 @@ const Hero = ({ backgroundColor, children }) => {
                 transition: 'background-color 0.5s ease-in-out'
             }}
         >
-            <div className="container-title">
+            <div className={`container-title ${isVisible ? 'preloader-main' : 'd-none'}`}>
                 <h1 className="d-inline-block justified">
                     Leonardo Cadore. Content producer, videomaker & photographer
                 </h1>
